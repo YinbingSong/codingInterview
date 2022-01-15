39c8448d5ecd030437c126cfd2d0406c894b4654
juno <song6151165@gmail.com>
2022-01-15 23:24:36 +0800

    面试金典1.4 - 1.6

--- String/String1.1/src/com/leetcode/Solution.java
+++ String/String1.1/src/com/leetcode/Solution.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
     public boolean isUnique(String astr){
@@ -62,4 +64,123 @@ public class Solution {
         return true;
     }
 
+    public String replaceSpaces(String S, int length) {
+        char[] chars = S.toCharArray();
+        int spacesNum = 0;
+        for (char aChar : chars) {
+            if (aChar == ' '){
+                spacesNum++;
+            }
+        }
+        if (spacesNum == 0) return S;
+
+        int realSize = length + spacesNum*2;
+        int orlSize = length - 1;
+
+        char[] ans = new char[realSize];
+        int i = realSize -1;
+        while (orlSize >= 0){
+            if (chars[orlSize] == ' ') {
+                ans[i--] = '0';
+                ans[i--] = '2';
+                ans[i--] = '%';
+            } else {
+                ans[i--] = chars[orlSize];
+            }
+            orlSize--;
+        }
+        return String.valueOf(ans, i + 1, S.length() - orlSize - 1);
+    }
+    public String replaceSpaces1(String S, int length) {
+        String substring = S.substring(0, length - 1);
+        return substring.replaceAll(" ", "%20");
+    }
+    public boolean canPermutePalindrome(String s) {
+        char[] chars = s.toCharArray();
+        HashMap<Character, Integer> characterIntegerHashMap = new HashMap<>();
+        for (Character c:chars){
+            if (characterIntegerHashMap.containsKey(c)) {
+                characterIntegerHashMap.put(c, characterIntegerHashMap.get(c) + 1);
+            }else {
+                characterIntegerHashMap.put(c, 1);
+            }
+        }
+        AtomicInteger count = new AtomicInteger();
+        characterIntegerHashMap.forEach((character, integer) -> {
+            if (integer % 2 == 1){
+                count.getAndIncrement();
+            }
+        });
+        return count.get() <= 1;
+    }
+
+    public boolean canPermutePalindrome1(String s){
+        HashSet<Character> hashSet = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            if (hashSet.add(c)) {
+            }else {
+                hashSet.remove(c);
+            }
+        }
+        return hashSet.size()<=1;
+    }
+
+    public boolean oneEditAway(String first, String second) {
+        int firstLength = first.length();
+        int secondLength = second.length();
+        if (firstLength - secondLength >1 || secondLength - firstLength > 1) return false;
+        if (firstLength == 0 || secondLength == 0) return true;
+        if (firstLength == secondLength){
+            return oneReplace(first,second);
+        }else if (firstLength == secondLength + 1){
+            return oneInsert(first,second);
+        }else {
+            return oneInsert(second, first);
+        }
+    }
+    private boolean oneReplace(String s1,String s2){
+        if (s1.length() != s2.length()) return false;
+        int temp = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (++temp > 1) return false;
+            }
+        }
+        return true;
+    }
+    private boolean oneInsert(String s1,String s2){
+        if (s1.length() - s2.length() != 1) return false;
+        int i = 0,temp = 0;
+        while (i < s2.length()){
+            if (s1.charAt(i + temp) != s2.charAt(i)) {
+                temp++;
+            }else {
+                i++;
+            }
+            if (temp > 1) return false;
+        }
+        return true;
+    }
+
+    public String compressString(String S) {
+        if (S.length() < 2) {
+            return S;
+        }
+        Character temp = S.charAt(0);
+        Integer tempNum = 0;
+//        StringBuilder ans = new StringBuilder();
+        StringBuffer ans = new StringBuffer();
+        char[] chars = S.toCharArray();
+        for (char aChar : chars) {
+            if (aChar == temp) {
+                tempNum++;
+            } else {
+                ans.append(temp).append(tempNum);
+                temp = aChar;
+                tempNum = 1;
+            }
+        }
+        ans.append(temp).append(tempNum);
+        return ans.length() > S.length() ? S : String.valueOf(ans);
+    }
 }
--- String/String1.1/test/com/leetcode/SolutionTest.java
+++ String/String1.1/test/com/leetcode/SolutionTest.java
@@ -8,10 +8,27 @@ public class SolutionTest {
 
     @Test
     public void isUnique() {
-        assertFalse(new Solution().isUnique("leetcode"));
-        assertTrue(new Solution().isUnique("abc"));
-        assertFalse(new Solution().isUnique1("leetcode"));
-        assertTrue(new Solution().isUnique1("abc"));
-        assertTrue(new Solution().isUnique1(""));
+//        assertFalse(new Solution().isUnique("leetcode"));
+//        assertTrue(new Solution().isUnique("abc"));
+//        assertFalse(new Solution().isUnique1("leetcode"));
+//        assertTrue(new Solution().isUnique1("abc"));
+//        assertTrue(new Solution().isUnique1(""));
+//        assertTrue(new Solution().canPermutePalindrome("aa"));
+//        assertFalse(new Solution().canPermutePalindrome("abca"));
+//        assertFalse(new Solution().canPermutePalindrome("abcabcddda"));
+//        assertTrue(new Solution().canPermutePalindrome("abcba"));
+//        assertTrue(new Solution().canPermutePalindrome("tactcoa"));
+//        assertTrue(new Solution().canPermutePalindrome1("aa"));
+//        assertFalse(new Solution().canPermutePalindrome1("abca"));
+//        assertFalse(new Solution().canPermutePalindrome1("abcabcddda"));
+//        assertTrue(new Solution().canPermutePalindrome1("abcba"));
+//        assertTrue(new Solution().canPermutePalindrome1("tactcoa"));
+//        assertTrue(new Solution().oneEditAway("ab","a"));
+//        assertTrue(new Solution().oneEditAway("adc","abc"));
+//        assertTrue(new Solution().oneEditAway("ac","abc"));
+//        assertFalse(new Solution().oneEditAway("abdd","abcc"));
+//        assertFalse(new Solution().oneEditAway("teacher","bleacher"));
+        assertEquals("a2b1c5a2",new Solution().compressString("aabcccccaa"));
+        assertEquals("a2b1c5a1",new Solution().compressString("aabccccca"));
     }
 }
