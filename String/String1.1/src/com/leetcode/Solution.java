bf62f1fe838e572fe1d3ffac97fda0a5cecb2059
juno <song6151165@gmail.com>
2022-06-30 19:20:12 +0800

    Add 1175.primeArrangements

--- String/String1.1/src/com/leetcode/Solution.java
+++ String/String1.1/src/com/leetcode/Solution.java
@@ -208,5 +208,33 @@ public class Solution {
         return s.substring(startIndex, startIndex + maxLength);
     }
 
+    public int numPrimeArrangements(int n) {
+        int primeNum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)){
+                primeNum++;
+            }
+        }
+        int res = (int) (factorial(primeNum) * factorial(n-primeNum) % MOD);
+        return res;
+    }
 
+    public boolean isPrime(int n){
+        if (n == 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if ( n % i == 0) return false;
+        }
+        return true;
+    }
+
+    static final int MOD = 1000000007;
+
+    public long factorial(int n){
+        long res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+            res %= MOD;
+        }
+        return res;
+    }
 }
